aa2e82fdf30efde0544ccc0c4cc849545e608744
Mateo Marulanda <marmacesal@gmail.com>
2020-08-21 03:11:23 -0500

    test

--- backend/test/databaseTest.js
+++ backend/test/databaseTest.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 before((done) => {
-  mongoose.connect("mongodb://localhost/app_test");
+  mongoose.connect("mongodb://localhost/app_test", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   mongoose.connection
     .once("open", () => {
       console.log("Connected to DB test");
